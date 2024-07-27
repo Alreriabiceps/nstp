cf9934228555e1a82c81daab46cc431a6c0babdf
justin <justin@redhq.com.au>
2024-07-27 15:16:24 +0400

    deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,3 +11,13 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Setting storage permissions..."
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
+
+
+echo "Running seeders..."
+php artisan db:seed --force
