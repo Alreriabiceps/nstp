3e9eb2349cadcfed641af4db428c33c8a751e96e
justin <justin@redhq.com.au>
2024-07-31 13:17:24 +0400

    Update authentication controllers to use username instead of email for validation

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -22,7 +22,7 @@ class NewPasswordController extends Controller
     public function create(Request $request): Response
     {
         return Inertia::render('Auth/ResetPassword', [
-            'email' => $request->email,
+            'username' => $request->username,
             'token' => $request->route('token'),
         ]);
     }
@@ -36,7 +36,7 @@ class NewPasswordController extends Controller
     {
         $request->validate([
             'token' => 'required',
-            'email' => 'required|email',
+            'username' => 'required',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
